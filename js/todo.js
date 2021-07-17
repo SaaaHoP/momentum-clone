b5b272fadbf63df710f66e6559a299adee5cff4a
SaaaHoP <qkrtjdgh97@gmail.com>
2021-07-17 16:24:21 +0900

    feat/reviseTodo
    투두목록 옆에 더보기 추가(기능X, 뜨는거만)

--- js/todo.js
+++ js/todo.js
@@ -6,21 +6,32 @@ let todoArr = [];
 const addTodo = (todoObject) => {
   const li = document.createElement('li');
   const span = document.createElement('span');
-  const button = document.createElement('button');
+  const buttonDelete = document.createElement('button');
+  const buttonRevise = document.createElement('button');
+  const deleteIcon = document.createElement('i');
+  const reviseIcon = document.createElement('i');
 
   li.className = todoObject.id;
   span.innerText = todoObject.text;
   span.classList.add('todo');
-  button.innerText = '❌';
+  deleteIcon.className = 'far fa-trash-alt';
+  buttonDelete.classList.add('delete-button');
+  reviseIcon.className = 'fas fa-ellipsis-h';
+  buttonRevise.classList.add('revise-button');
+
   if (todoObject.clicked) {
     span.classList.add('todoClicked');
   } else {
     span.classList.remove('todoClicked');
   }
-  button.addEventListener('click', deleteTodo);
+  buttonDelete.addEventListener('click', deleteTodo);
+  buttonRevise.addEventListener('click', editTodo);
   span.addEventListener('click', todoClicked);
   li.appendChild(span);
-  li.appendChild(button);
+  li.appendChild(buttonDelete);
+  buttonDelete.appendChild(deleteIcon);
+  li.appendChild(buttonRevise);
+  buttonRevise.appendChild(reviseIcon);
   todoList.appendChild(li);
 };
 
@@ -39,7 +50,7 @@ const todoClicked = (e) => {
 
 //todo 삭제하기
 const deleteTodo = (e) => {
-  const selectedLi = e.target.parentElement;
+  const selectedLi = e.currentTarget.parentElement;
   selectedLi.remove();
   todoArr = todoArr.filter((todoObject) => {
     //Date.now()는 정수, class와 id는 문자열이여서 parseInt 혹은 toString 써줘야한다.
@@ -47,6 +58,62 @@ const deleteTodo = (e) => {
   });
   localStorageSet();
 };
+
+//todo 수정창 열기
+const editTodo = (e) => {
+  const reviseBox = document.createElement('div');
+  const reviseButton = document.createElement('button');
+  const reviseIcon = document.createElement('i');
+  const highlightButton = document.createElement('button');
+  const highlightIcon = document.createElement('i');
+  reviseBox.className = 'arrow-box';
+  reviseIcon.className = 'far fa-edit';
+  reviseIcon.classList.add('reviseIcon');
+  highlightIcon.className = 'fas fa-highlighter';
+  highlightIcon.classList.add('highlightIcon');
+  document.body.appendChild(reviseBox);
+  // e.currentTarget.parentElement.appendChild(reviseBox);
+
+  reviseBox.appendChild(reviseButton);
+  reviseBox.appendChild(highlightButton);
+  reviseButton.appendChild(reviseIcon);
+  highlightButton.appendChild(highlightIcon);
+  reviseButton.addEventListener('click', reviseTodo);
+
+  reviseBox.style.left = e.currentTarget.offsetLeft + 20 + 'px';
+  reviseBox.style.top = e.currentTarget.offsetTop - 10 + 'px';
+  reviseBox.classList.add('arrow-box-reveal');
+};
+
+//todo 수정하기
+const reviseTodo = (e) => {};
+
+//화면 밖 클릭시 수정화면 끄기
+document.body.addEventListener('click', (e) => {
+  const reviseBox = document.querySelectorAll('.arrow-box');
+  console.log(e.target.parentElement);
+  if (
+    e.target == e.currentTarget.querySelector('.arrow-box') ||
+    e.target == e.currentTarget.querySelector('.reviseIcon') ||
+    e.target == e.currentTarget.querySelector('.highlightIcon')
+  ) {
+    e.currentTarget.querySelector('.arrow-box').style.opacity = '1';
+    console.log('안쪽클릭1');
+    return;
+  }
+
+  if (
+    e.target.parentElement == e.currentTarget.querySelector('.revise-button')
+  ) {
+    e.currentTarget.querySelector('.arrow-box').style.opacity = '1';
+    console.log('안쪽클릭2');
+    return;
+  }
+
+  reviseBox.forEach((item) => {
+    item.style.opacity = '0';
+  });
+});
 
 const localStorageSet = () => {
   localStorage.setItem('todoArr', JSON.stringify(todoArr));
